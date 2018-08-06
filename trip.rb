0a508b6cf6c753343dd7f7ce6697e64fd450320a
chrisssxuu <chris01.xu@gmail.com>
2018-08-06 19:16:42 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -17,4 +17,4 @@ nights = gets.chomp
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts "This person will be staying at #{stay} and eating at #{eats} and staying for #{nights} nights."
+puts "This person will be staying at #{stay} and eating  #{eats} and staying for #{nights} nights."
