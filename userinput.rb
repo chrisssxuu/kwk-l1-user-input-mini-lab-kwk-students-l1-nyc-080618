6774bd062a21713d67d33f91cce856d531df1b6d
chrisssxuu <chris01.xu@gmail.com>
2018-08-06 19:14:20 +0000

    automatically backed up by learn

--- userinput.rb
+++ userinput.rb
@@ -4,4 +4,4 @@ name = gets.chomp.capitalize
 puts "What is your favorite food?"
 food = gets.chomp
 
-puts "My name is #{name} and my favorite food is #{food}"
+puts "My name is #{name} and my favorite food is #{food}."
