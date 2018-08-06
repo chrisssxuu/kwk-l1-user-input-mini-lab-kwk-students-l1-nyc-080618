aee3ff28e75c54065f6843f8a277d39a6f985849
chrisssxuu <chris01.xu@gmail.com>
2018-08-06 19:08:15 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -17,4 +17,4 @@ nights = gets.chomp.capitalize
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts "When visiting Princeton, this person will be staying at #{stay} and eating at #{eats} and staying for #{nights}."
+puts "This person will be staying at #{stay} and eating at #{eats} and staying for #{nights}."
